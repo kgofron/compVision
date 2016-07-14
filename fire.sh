df2aa7eee3c12fd084a3d3cdc602ad15c1dd6543
William Watson <wwatson@xf10id-ws3.cs.nsls2.local>
2016-07-14 09:28:46 -0400

    Update Files In Case Of Fire at 09:28:46 - 07/14/16

--- fire.sh
+++ fire.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+git pull
+git log --graph --abbrev-commit --decorate --date=relative --all > gitlog.txt
+git status
 git add -A
 now=$(date +"%T - %D")
 git commit -m "Update Files In Case Of Fire at $now"
