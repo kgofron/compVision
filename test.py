daeddf85747c3292252aa181e7216e2eaa12749f
William Watson <wwatson@localhost.localdomain>
2016-07-15 14:56:09 -0400

    Update Files In Case Of Fire at 14:56:09 - 07/15/16

--- test.py
+++ test.py
@@ -1,15 +1,42 @@
 import cvlib
 from matplotlib import pyplot as plt
 import cv2
-img = cvlib.fetchImg("XF:10IDD-BI", "Mir:HFM-Cam:1")
-img2 = cvlib.fetchImg("XF:10IDD-BI", "Mir:VFM-Cam:1")
+from time import sleep
+count = 0
+lst = []
+while count < 3:
+    img = cvlib.fetchImg("XF:10IDD-BI", "Mir:HFM-Cam:1")
+    cvlib.save(img, "HFM%d.tif" % count)
+    #img = cvlib.load("HFM%d.tif" % count)
+    lap = cvlib.binaryThreshold(img, threshVal=100)
+    contours = cvlib.findContours(lap)
+    
+    
+    cvlib.fillContour(lap, contours[0], color=(100,100,255)) 
+    
+    m = cvlib.cntInfo(img, contours[0])
+    cvlib.plotPoints(lap, cvlib.extremePointsTup(contours[0]), radius=8)
+    cvlib.drawContour(lap, contours[0])
+    cvlib.plotPoints(img, cvlib.extremePointsTup(contours[0]), radius=8)
+    cvlib.drawContour(img, contours[0])
+    cvlib.plotPoint(img, m["max"], color=(100,100,0), radius=5)
+    cvlib.plotPoint(lap, m["max"], color=(100,100,0), radius=5)
+    cvlib.plotCentroid(lap, contours[0])
+    print "Object Details: " + str(m)
+    cvlib.display(img)
+    jet = cvlib.applyColorMap(img, "jet")
+    lst.append(jet)
+    
+    count = count + 1
+
+#img2 = cvlib.fetchImg("XF:10IDD-BI", "Mir:VFM-Cam:1")
 #img = cv2.imread("T20160715.jpg")
 
-print len(img)
-print len(img2)
+#print len(img)
+#print len(img2)
 
-cvlib.save(img, "HFM.jpg")
-cvlib.save(img2, "VFM.jpg")
+
+#cvlib.save(img2, "VFM.jpg")
 
 """
 lap = cvlib.binaryThreshold(img, threshVal=100)
