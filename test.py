b51a71e1544bfdbe98e87fcdcec0287a0f35b24f
William Watson <wwatson@localhost.localdomain>
2016-07-15 13:27:06 -0400

    Update Files In Case Of Fire at 13:27:06 - 07/15/16

--- test.py
+++ test.py
@@ -4,7 +4,9 @@ from matplotlib import pyplot as plt
 img = cvlib.fetchImg("XF:10IDD-BI", "OnAxis-Cam:1")
 
 #img = cvlib.load("selfie.jpg")
-cvlib.display(img)
+#cvlib.display(img)
+print img
+cvlib.save(img)
 """
 lap = cvlib.binaryThreshold(img, threshVal=100)
 contours = cvlib.findContours(lap)
