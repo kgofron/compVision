0b8cefd6bb6582497d6f0699a19276cd91e6c6ba
William Watson <wwatson@localhost.localdomain>
2016-07-18 12:29:52 -0400

    Update Files In Case Of Fire at 12:29:52 - 07/18/16

--- IXS/BPM2Proc.py
+++ IXS/BPM2Proc.py
@@ -1,7 +1,7 @@
 import cvlib
 
 img = cvlib.load("BPM2-0.jpg") # change to fetch
-lap = cvlib.binaryThreshold(img, threshVal=170)
+lap = cvlib.binaryThreshold(img, threshVal=253)
 contours = cvlib.findContours(lap)
 cvlib.fillContour(lap, contours[0], color=(100,100,255))
 
@@ -10,10 +10,10 @@ cvlib.plotPoints(lap, cvlib.extremePointsTup(contours[0]), radius=8)
 cvlib.drawContour(lap, contours[0])
 cvlib.plotPoints(img, cvlib.extremePointsTup(contours[0]), radius=8)
 cvlib.drawContour(img, contours[0])
-cvlib.plotPoint(img, m["max"], color=(100,100,0), radius=5)
-cvlib.plotPoint(lap, m["max"], color=(100,100,0), radius=5)
-cvlib.plotCentroid(lap, contours[0])
-cvlib.plotCentroid(img, contours[0])
+cvlib.plotPoint(img, m["max"], color=(100,100,0), radius=8)
+cvlib.plotPoint(lap, m["max"], color=(100,100,0), radius=8)
+cvlib.plotCentroid(lap, contours[0], radius=10)
+cvlib.plotCentroid(img, contours[0], radius=10)
 print "Object Details: " + str(m)
 jet = cvlib.applyColorMap(img, "jet")
 
