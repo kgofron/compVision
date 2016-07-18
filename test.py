43f302dfea966bd68faa0baf8bfa1c1b20603422
William Watson <wwatson@localhost.localdomain>
2016-07-18 09:08:09 -0400

    Update Files In Case Of Fire at 09:08:09 - 07/18/16

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ count = 0
 lst = []
 while count < 3:
     img = cvlib.fetchImg("XF:10IDD-BI", "Mir:HFM-Cam:1")
-    cvlib.save(img, "HFMT%d.tiff" % count)
+    cvlib.save(img, "HFMT%d.jpg" % count)
     #img = cvlib.load("HFM%d.tif" % count)
     """lap = cvlib.binaryThreshold(img, threshVal=100)
     contours = cvlib.findContours(lap)
