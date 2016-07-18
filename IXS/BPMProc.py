16534d999679189b211367f415118d93f4cd3392
William Watson <wwatson@xf10id-ws3.cs.nsls2.local>
2016-07-18 14:35:09 -0400

    Update Files In Case Of Fire at 14:35:09 - 07/18/16

--- IXS/BPMProc.py
+++ IXS/BPMProc.py
@@ -13,6 +13,7 @@ cvlib.drawContour(img, contours[0])
 cvlib.plotPoint(img, m["max"], color=(100,100,0), radius=5)
 cvlib.plotPoint(lap, m["max"], color=(100,100,0), radius=5)
 cvlib.plotCentroid(lap, contours[0])
+cvlib.plotCentroid(img, contours[0])
 print "Object Details: " + str(m)
 jet = cvlib.applyColorMap(img, "jet")
 
