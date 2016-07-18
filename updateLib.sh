cba6db2ceda8c083e49166eb4e10ea264a13441d
William Watson <wwatson@xf10id-ws3.cs.nsls2.local>
2016-07-18 12:47:34 -0400

    Update Files In Case Of Fire at 12:47:34 - 07/18/16

--- updateLib.sh
+++ updateLib.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+printf "Copying Files into Sub Directories..."
 cp cvlib.py Crystals/cvlib.py
 cp cvlib.py Merlin/cvlib.py
 cp cvlib.py RawImages/cvlib.py
@@ -7,7 +8,7 @@ cp cvlib.py Showcase/cvlib.py
 cp cvlib.py XrayImg/cvlib.py
 cp cvlib.py CV\ Library/cvlib.py
 cp cvlib.py OnAxisImg/cvlib.py
-
+cp cvlib.py IXS/cvlib.py
 printf "\n"
 printf "All Folders updated with new cvlib.py file..."
 printf "\n"
