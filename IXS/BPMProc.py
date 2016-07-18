b476a4e043a585f39eb0ef78b661cfce38744e6e
William Watson <wwatson@xf10id-ws3.cs.nsls2.local>
2016-07-18 14:33:28 -0400

    Update Files In Case Of Fire at 14:33:28 - 07/18/16

--- IXS/BPMProc.py
+++ IXS/BPMProc.py
@@ -1,7 +1,7 @@
 import cvlib
 
 img = cvlib.load("BPM1-0.jpg") # change to fetch
-lap = cvlib.binaryThreshold(img, threshVal=20)
+lap = cvlib.binaryThreshold(img, threshVal=90)
 contours = cvlib.findContours(lap)
 cvlib.fillContour(lap, contours[0], color=(100,100,255))
 
