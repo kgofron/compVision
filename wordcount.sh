f7a8f00423ad8761aa17f27c1bfc9dfac84d8c57
William Watson <wwatson@localhost.localdomain>
2016-07-18 13:12:10 -0400

    Update Files In Case Of Fire at 13:12:10 - 07/18/16

--- wordcount.sh
+++ wordcount.sh
@@ -3,5 +3,5 @@
 printf "TOTAL WORD COUNT:\n"
 git ls-files | xargs wc -l
 printf "\nPython Files Word Count:\n"
-git ls-files | grep '.py\|.sh\|.md\|.txt' | grep -v '.pyc' | xargs wc -l
+git ls-files | grep '.py\|.sh\|.md\|.txt\|LICENSE' | grep -v '.pyc' | xargs wc -l
 
