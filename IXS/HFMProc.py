2f7420642a2248a5770d30103b0412fb1332657f
William Watson <wwatson@localhost.localdomain>
2016-07-18 13:53:15 -0400

    Update Files In Case Of Fire at 13:53:15 - 07/18/16

--- IXS/HFMProc.py
+++ IXS/HFMProc.py
@@ -3,4 +3,5 @@ import cvlib
 slit = cvlib.load("slitHFM.jpg")
 img = cvlib.load("HFM0.jpg") #change to fetch
 img = cvlib.drawMatch(img, slit) # SHould probably retriueve info on pos
+pts = cvlib.templateMatchSingle(img, slit)
 cvlib.display(img)
--- IXS/VFMProc.py
+++ IXS/VFMProc.py
@@ -3,4 +3,6 @@ import cvlib
 slit = cvlib.load("slitVFM.jpg")
 img = cvlib.load("VFM0.jpg") # Change to fetch
 img = cvlib.drawMatch(img, slit) # SHould probably retriueve info on pos
+pts = cvlib.templateMatchSingle(img, slit)
+print pts
 cvlib.display(img)
