dfa01692383b6862c44f8f3da9bf8ec376011b34
William Watson <wwatso13@jhu.edu>
2016-07-28 07:52:45 -0400

    Update Files In Case Of Fire at 07:52:45 - 07/28/16

--- AMX/Crystal/crystalCentering.py
+++ AMX/Crystal/crystalCentering.py
@@ -9,11 +9,11 @@ for i in range(24): #24
     rng = cvlib.inRangeThresh(img, (20,30,20), (200,130,120))
     rng = cvlib.bitNot(rng)
     cnt = cvlib.findContours(rng, thresh=250)
-    centroid = cvlib.centroid(crystal)
     if cvlib.area(cnt[0]) > cvlib.area(cnt[1]):
         crystal = cnt[0]
     else:
         crystal = cnt[1]
+    centroid = cvlib.centroid(crystal)
     center.append(centroid[1])
     cvlib.drawContour(img, crystal, thickness=10)
     cvlib.plotCentroid(img, crystal, radius=7)
@@ -21,11 +21,14 @@ for i in range(24): #24
     cvlib.save(img, "found%d.jpg" % angle)
     angle += 15
 
-cvlib.saveGraph(angles, center, "Y Coord Per Angle", "Angles in Degrees", "Original Data Coord", [0,360,0,400], filename="graph.png")
+cvlib.saveGraph(angles, center, "Y Coord Per Angle", "Angle in Degrees", "Original Data Coord", [0,360,0,400], filename="graph.png")
 d = cvlib.approxSinCurve(center)
+print angles
+print center
+print "\n"
 print d["amplitude"], d["phase shift"], d["vertical shift"]
-cvlib.saveGraph(angles, d["data"], "Y Coord Per Angle", "Angles in Degrees", "Y Coord Centroid Best Fit", [0,360,0,400], style="--", filename="fit.png")
-cvlib.makeGraph(angles, d["data"], "Y Coord Per Angle", "Angles in Degrees", "Y Coord Centroid", [0,360,0,400], style="r--")
+cvlib.saveGraph(angles, d["data"], "Y Coord Per Angle", "Angle in Degrees", "Y Coord Centroid Best Fit", [0,360,0,400], style="--", filename="fit.png")
+cvlib.makeGraph(angles, d["data"], "Y Coord Per Angle", "Angle in Degrees", "Y Coord Centroid", [0,360,0,400], style="r--")
 # X = -[mc/pel]*A*sin(phase)
 # Y = -[mc/pel]*A*cos(phase)
 
