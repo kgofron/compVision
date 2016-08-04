584a5c8e892f646c203fea6647465c5fba2c7112
Bill Watson <wwatso13@jhu.edu>
2016-08-04 16:25:33 -0400

    Add files via upload

--- AMX/Crystal/epicsCrystalCentering.py
+++ AMX/Crystal/epicsCrystalCentering.py
@@ -6,8 +6,8 @@ DEV = "Cam:7"
 MOTOR = "XF:17IDB-ES:AMX{Gon:1-Ax:O}Mtr"
 YMOTOR = "XF:17IDB-ES:AMX{Gon:1-Ax:PY}Mtr"
 ZMOTOR = "XF:17IDB-ES:AMX{Gon:1-Ax:PZ}Mtr"
-YMTRSCALE = 100.0/120.0
-ZMTRSCALE = 100.0/120.0
+YMTRSCALE = 0.859375  #100.0/120.0
+ZMTRSCALE = 0.849609375  #100.0/120.0
 NumImgs = 33
 
 angle = cvlib.caget(MOTOR+".RBV")
@@ -40,15 +40,15 @@ for i in range(NumImgs):
         crystal = cnt[1]"""
     centroid = cvlib.centroid(crystal)
     center.append(centroid[1])
-    #cvlib.drawContour(img, crystal, thickness=10)
-    #cvlib.plotCentroid(img, crystal, radius=7)
-    #cvlib.save(img, "foundEPICS%d.jpg" % angle)
+    cvlib.drawContour(img, crystal, thickness=10)
+    cvlib.plotCentroid(img, crystal, radius=7)
+    cvlib.save(img, "foundEPICS%d.jpg" % angle)
 
-#cvlib.saveGraph(angles, center, "Y Coord Per Angle", "Angle in Degrees", "Original Data Coord", filename="graph.png")
+cvlib.saveGraph(angles, center, "Y Coord Per Angle", "Angle in Degrees", "Original Data Coord", filename="graph.png")
 d = cvlib.approxSinCurve(angles, center)
-#print d["amplitude"], d["phase shift"], d["vertical shift"]
+print d["amplitude"], d["phase shift"], d["vertical shift"]
 
-#cvlib.saveGraph(angles, d["data"], "Y Coord Per Angle", "Angle in Degrees", "Y Coord Centroid Best Fit", style="r--", filename="fit.png")
+cvlib.saveGraph(angles, d["data"], "Y Coord Per Angle", "Angle in Degrees", "Y Coord Centroid Best Fit", style="r--", filename="fit.png")
 #cvlib.makeGraph(angles, d["data"], "Y Coord Per Angle", "Angle in Degrees", "Y Coord Centroid", style="r--")
 
 
@@ -59,6 +59,10 @@ cvlib.caput(ZMOTOR+".RLV", -ZMTRSCALE*d["amplitude"]*np.cos(d["phase shift"]*np.
 
 cvlib.caput(MOTOR+".VAL", 0)
 while cvlib.caget(MOTOR+".DMOV") != 1:
+    angle = cvlib.caget(MOTOR+".RBV")
+    img = cvlib.fetchImg(SYS, DEV) 
+    img = np.array(img, np.uint8)
+    cvlib.save(img, "EPICS%d.jpg" % angle)
     continue
 
 # X = -[mc/pel]*A*sin(phase)
