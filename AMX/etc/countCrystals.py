bfb134c605c6503a445340ae6704706f9242d744
William Watson <wwatso13@jhu.edu>
2016-08-04 18:34:48 -0400

    attempt

--- AMX/etc/countCrystals.py
+++ AMX/etc/countCrystals.py
@@ -1,7 +1,13 @@
 import cvlib
 
 img = cvlib.load("comprec1-top3.jpg")
-tmp = img.copy()
-
-
-cvlib.display(img)
+#for i in range(30):
+tmp = cvlib.grayscale(img)
+a = cvlib.floodFill(tmp, (2200,300), val=0, lo=1, hi=1)
+a = cvlib.opening(a)
+m = cvlib.minMaxLoc(tmp)
+print m
+cvlib.plotPoints(img, [m["minLoc"], m["maxLoc"]], radius = 100)
+#tmp = cvlib.binaryThreshold(tmp, threshVal=80)
+cvlib.matplotlibDisplay(a)
+cvlib.save(tmp, "gray.jpg")
